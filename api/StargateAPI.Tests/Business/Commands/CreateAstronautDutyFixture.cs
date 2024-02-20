9e8f265322efe7c30257c33015370461befdb325
Anthony Travisano <A.R.Travisano@eagle.clarion.edu>
2024-02-19 21:49:16 -0500

    Write first unit test

---
+++ api/StargateAPI.Tests/Business/Commands/CreateAstronautDutyFixture.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using StargateAPI.Business.Commands;
+using StargateAPI.Business.Data;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StargateAPI.Tests.Business.Commands
+{
+    public class CreateAstronautDutyFixture : IDisposable
+    {
+        private readonly DbConnection _connection;
+        private readonly DbContextOptions<StargateContext> _contextOptions;
+
+        public CreateAstronautDutyFixture()
+        {
+            _connection = new SqliteConnection("Filename=:memory:");
+            _connection.Open();
+
+            _contextOptions = new DbContextOptionsBuilder<StargateContext>()
+                .UseSqlite(_connection)
+                .Options;
+
+            using var context = CreateContext();
+            context.Database.EnsureCreated();
+        }
+
+        [Fact]
+        public async void Process_NoPeople_ShouldThrowException()
+        {
+            // Arrange
+            using var context = CreateContext();
+            var preprocessor = CreatePreProcessor(context);
+            var request = new CreateAstronautDuty
+            {
+                Name = "unit test name",
+                DutyTitle = "unit test duty title",
+                Rank = "unit test rank"
+            };
+
+            // Act
+            var action = () => preprocessor.Process(request, new CancellationToken());
+
+            // Assert
+            var result = await Assert.ThrowsAsync<BadHttpRequestException>(action);
+            Assert.Equal("Bad Request", result.Message);
+        }
+
+        public void Dispose()
+        {
+            _connection.Dispose();
+        }
+
+        private StargateContext CreateContext()
+        {
+            return new(_contextOptions);
+        }
+
+        private CreateAstronautDutyPreProcessor CreatePreProcessor(StargateContext context)
+        {
+            return new CreateAstronautDutyPreProcessor(context);
+        }
+    }
+
+}
